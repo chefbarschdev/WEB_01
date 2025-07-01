9bf0b576a30b481937bbfc21c1978abb3c6a5e52
chefbarschdev <timnewlink@gmail.com>
2025-07-02 00:34:19 +0200

    Remove autoFocus from waitlist name input

--- src/components/WaitlistForm.tsx
+++ src/components/WaitlistForm.tsx
@@ -146,7 +146,6 @@ export const WaitlistForm = component$(() => {
                 class="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-green-500 focus:border-transparent"
                 placeholder="John Smith"
                 autoComplete="name"
-                autoFocus
                 required
               />
             </div>
