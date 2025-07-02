0f10f6a951328a752013c66c7ba2d9ccbc4f549b
chefbarschdev <timnewlink@gmail.com>
2025-07-02 06:11:33 +0200

    chore: format key files with Prettier

--- src/root.tsx
+++ src/root.tsx
@@ -1,19 +1,29 @@
-import { component$ } from '@builder.io/qwik';
+import { component$ } from "@builder.io/qwik";
 import {
   QwikCityProvider,
   RouterOutlet,
   ServiceWorkerRegister,
-} from '@builder.io/qwik-city';
-import { RouterHead } from './components/router-head/router-head';
-import { isDev } from '@builder.io/qwik';
+} from "@builder.io/qwik-city";
+import { RouterHead } from "./components/router-head/router-head";
+import { isDev } from "@builder.io/qwik";
 
-import './global.css';
+import "./global.css";
 
 export default component$(() => {
   return (
     <QwikCityProvider>
       <head>
         <meta charset="utf-8" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
         {!isDev && (
           <link
             rel="manifest"
